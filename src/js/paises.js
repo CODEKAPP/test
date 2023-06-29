79b6008f576301727cc844098236fd08dcb957aa
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-06-29 17:55:11 -0500

    filtro_pais

---
+++ src/js/paises.js
@@ -0,0 +1,86 @@
+export const selecPaises= [
+    {clave:"*", nombre: "Choose country"},
+    {clave:"ad", nombre: "Andorra"},//no
+    {clave:"us", nombre: "United States Of America"},// si
+    {clave:"ai", nombre: "Anguilla"}, // No
+    {clave:"ar", nombre: "Argentina"},// No
+    {clave:"au", nombre: "Australia"},// si
+    {clave:"at", nombre: "Austria"}, // No
+    {clave:"az", nombre: "Azerbaijan"}, // No
+    {clave:"bs", nombre: "Bahamas"}, // No
+    {clave:"bh", nombre: "Bahrain"}, // No
+    {clave:"bb", nombre: "Barbados"}, // No
+    {clave:"be", nombre: "Belgium"}, // No
+    {clave:"bm", nombre: "Bermuda"}, // No
+    {clave:"br", nombre: "Brazil"}, // No
+    {clave:"bg", nombre: "Bulgaria"}, // No
+    {clave:"ca", nombre: "Canada"}, //Si
+    {clave:"cl", nombre: "Chile"}, // No
+    {clave:"cn", nombre: "China"}, // No
+    {clave:"co", nombre: "Colombia"}, // No
+    {clave:"cr", nombre: "Costa Rica"}, // No
+    {clave:"hr", nombre: "Croatia"}, // No
+    {clave:"cy", nombre: "Cyprus"}, // No
+    {clave:"cz", nombre: "Czech Republic"}, // No
+    {clave:"dk", nombre: "Denmark"}, // No
+    {clave:"do", nombre: "Dominican Republic"}, // No
+    {clave:"ec", nombre: "Ecuador"}, // No
+    {clave:"ee", nombre: "Estonia"}, // No
+    {clave:"fo", nombre: "Faroe Islands"}, // No
+    {clave:"fi", nombre: "Finland"}, // No
+    {clave:"fr", nombre: "France"}, // No
+    {clave:"ge", nombre: "Georgia"}, // No
+    {clave:"de", nombre: "Germany"}, //Si
+    {clave:"gh", nombre: "Ghana"}, // No
+    {clave:"gi", nombre: "Gibraltar"}, // No
+    {clave:"gb", nombre: "Great Britain"}, //Si
+    {clave:"gr", nombre: "Greece"}, // No
+    {clave:"hk", nombre: "Hong Kong"}, // No
+    {clave:"hu", nombre: "Hungary"}, // No
+    {clave:"is", nombre: "Iceland"}, // No
+    {clave:"in", nombre: "India"}, // No
+    {clave:"ie", nombre: "Ireland"}, // No
+    {clave:"il", nombre: "Israel"}, // No
+    {clave:"it", nombre: "Italy"}, // No
+    {clave:"jm", nombre: "Jamaica"}, // No
+    {clave:"jp", nombre: "Japan"}, // No
+    {clave:"kr", nombre: "Korea, Republic of"}, // No
+    {clave:"lv", nombre: "Latvia"}, // No
+    {clave:"lb", nombre: "Lebanon"}, // No
+    {clave:"lt", nombre: "Lithuania"}, // No
+    {clave:"lu", nombre: "Luxembourg"}, // No
+    {clave:"my", nombre: "Malaysia"}, // No
+    {clave:"mt", nombre: "Malta"}, // No
+    {clave:"mx", nombre: "Mexico"},// Si
+    {clave:"mc", nombre: "Monaco"},  // No
+    {clave:"me", nombre: "Montenegro"}, // No
+    {clave:"ma", nombre: "Morocco"}, // No
+    {clave:"nl", nombre: "Netherlands"}, // No
+    {clave:"an", nombre: "Netherlands Antilles"}, // No
+    {clave:"nz", nombre: "New Zealand"},// Si
+    {clave:"nd", nombre: "Northern Ireland"}, // No
+    {clave:"no", nombre: "Norway"}, // No
+    {clave:"pe", nombre: "Peru"}, // No
+    {clave:"pl", nombre: "Poland"}, // No
+    {clave:"pt", nombre: "Portugal"}, // No
+    {clave:"ro", nombre: "Romania"}, // No
+    {clave:"ru", nombre: "Russian Federation"}, // No
+    {clave:"lc", nombre: "Saint Lucia"}, // No
+    {clave:"sa", nombre: "Saudi Arabia"}, // No
+    {clave:"rs", nombre: "Serbia"}, // No
+    {clave:"sg", nombre: "Singapore"}, // No
+    {clave:"sk", nombre: "Slovakia"}, // No
+    {clave:"si", nombre: "Slovenia"}, // No
+    {clave:"za", nombre: "South Africa"}, // No
+    {clave:"es", nombre: "Spain"}, //si
+    {clave:"se", nombre: "Sweden"}, // No
+    {clave:"ch", nombre: "Switzerland"}, // No
+    {clave:"tw", nombre: "Taiwan"}, // No
+    {clave:"th", nombre: "Thailand"}, // No
+    {clave:"tt", nombre: "Trinidad and Tobago"}, // No
+    {clave:"tr", nombre: "Turkey"}, // No
+    {clave:"ua", nombre: "Ukraine"}, // No
+    {clave:"ae", nombre: "United Arab Emirates"}, // No
+    {clave:"uy", nombre: "Uruguay"}, // No
+    {clave:"ve", nombre: "Venezuela"} // No
+  ]
